7c44efb773e58664596898a2ab130106e0c68603
359762406 <359762406@qq.com>
2023-08-14 15:56:36 +0800

    完成 string API

--- Chapter11_API/Chapter11_API/src/com/qihang01/string/StringTest.java
+++ Chapter11_API/Chapter11_API/src/com/qihang01/string/StringTest.java
@@ -20,7 +20,8 @@ package com.qihang01.string;
  *String的不可变性
  * 当对字符串变量重新赋值时，需要重新指定一个字符串常量的位置进行赋值，不能在原有的位置修改
  * 当对现有的字符串进行拼接操作时，需要重新开辟空间保存拼接以后的字符串，不能在原有的位置修阈
- *
+ * 当调用字符串的replace()替换现有的某个字符时，
+ *   需要重新开辟空间保存修改以后的字符串，不能在原有的位置修改
  */
 public class StringTest {
     public static void main(String[] args) {
@@ -28,8 +29,10 @@ public class StringTest {
         String s2 = "hello";  // 存放
         System.out.println(s1 == s2);
         s2 = "hi" + "123";
+
+        String s3 = s1.replace('l', 'w');
         System.out.println(s2);
-        System.out.println(s1);
-        s1.replace('l', 'w');
+        System.out.println(s2);
+        System.out.println(s3);
     }
 }
