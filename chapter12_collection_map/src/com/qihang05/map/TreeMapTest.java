16d6d7781a0fdd90be09ecbf0a7004845d73aaef
359762406 <359762406@qq.com>
2023-08-16 21:07:18 +0800

    完成map

--- chapter12_collection_map/src/com/qihang05/map/TreeMapTest.java
+++ chapter12_collection_map/src/com/qihang05/map/TreeMapTest.java
@@ -2,6 +2,7 @@ package com.qihang05.map;
 
 import org.junit.Test;
 
+import java.util.Comparator;
 import java.util.Set;
 import java.util.TreeMap;
 import java.util.TreeSet;
@@ -36,7 +37,28 @@ public class TreeMapTest {
     }
     @Test
     public void test2(){
-        TreeMap map = new TreeMap();
+        Comparator comparator = new Comparator() {
+            @Override
+            public int compare(Object o1, Object o2) {
+                System.out.println("Comparator...");
+                if (o1 == o2){
+                    return 0;
+                }
+                if (o1 instanceof Person && o2 instanceof Person){
+                    Person p1 = (Person) o1;
+                    Person p2 = (Person) o2;
+                    int value = p1.getName().compareTo(p2.getName());
+                    if (value != 0){
+                        return value;
+                    }else {
+                        return -(p1.getAge() - p2.getAge());
+                    }
+                }
+                throw new RuntimeException("类型不匹配");
+            }
+        };
+
+        TreeMap map = new TreeMap(comparator);
         Person p1 = new Person(1231, "das");
         Person p2 = new Person(12131, "das");
         Person p3 = new Person(1231, "aas");
