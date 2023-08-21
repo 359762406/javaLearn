6d0aa550744e96fe31d305910d86fc247168f5db
359762406 <359762406@qq.com>
2023-08-21 11:51:21 +0800

    hashMap解析

--- chapter14_dataStructure/src/com/qihang03/map/HashMapTest.java
+++ chapter14_dataStructure/src/com/qihang03/map/HashMapTest.java
@@ -21,8 +21,8 @@ public class HashMapTest {
       使用数组的目的是查询和添加的效率高，可以根据索引直接定位到某个table[index]。
     * */
     @Test
-    public void test1(){
-        HashMap<String, Integer> hashMap= new HashMap<>();
+    public void test1() {
+        HashMap<String, Integer> hashMap = new HashMap<>();
     }
 
     /**
@@ -55,6 +55,185 @@ if ((size >= threshold) && (null != table[bucketIndex])) :
 补充：jdk7源码中定义的：
 static class Entry<K,V> implements Map.Entry<K,V>
 */
+    @Test
+    public void test(){
+
+    }
+    //key-value被封装为HashMap.Entry类型，而这个类型实现了Map.Entry接口。
+    /*
+    public class HashMap<K,V>{
+    transient Entry<K,V>[] table;
+
+    static class Entry<K,V> implements Map.Entry<K,V> {
+        final K key;
+        V value;
+        Entry<K,V> next;
+        int hash;
+
+
+         * Creates new entry.
+
+        Entry(int h, K k, V v, Entry<K,V> n) {
+            value = v;
+            next = n;
+            key = k;
+            hash = h;
+        }
+        //略
+    }
+    }
+
+    * */
+    //属性
+    /*
+    *
+    * //table数组的默认初始化长度
+    static final int DEFAULT_INITIAL_CAPACITY = 16;
+    //哈希表
+    transient Entry<K,V>[] table;
+    //哈希表中key-value的个数
+    transient int size;
+    //临界值、阈值（扩容的临界值）
+    int threshold;
+    //加载因子
+    final float loadFactor;
+    //默认加载因子
+    static final float DEFAULT_LOAD_FACTOR = 0.75f;
+    * */
+    //构造器
+    /*
+    * public HashMap() {
+        //DEFAULT_INITIAL_CAPACITY：默认初始容量16
+        //DEFAULT_LOAD_FACTOR：默认加载因子0.75
+        this(DEFAULT_INITIAL_CAPACITY, DEFAULT_LOAD_FACTOR);
+      }
+      public HashMap(int initialCapacity, float loadFactor) {
+        //校验initialCapacity合法性
+        if (initialCapacity < 0)
+            throw new IllegalArgumentException("Illegal initial capacity: " + initialCapacity);
+        //校验initialCapacity合法性
+        if (initialCapacity > MAXIMUM_CAPACITY)
+            initialCapacity = MAXIMUM_CAPACITY;
+        //校验loadFactor合法性
+        if (loadFactor <= 0 || Float.isNaN(loadFactor))
+            throw new IllegalArgumentException("Illegal load factor: " + loadFactor);
+
+        //计算得到table数组的长度（保证capacity是2的整次幂）
+        int capacity = 1;
+        while (capacity < initialCapacity)
+            capacity <<= 1;
+        //加载因子，初始化为0.75
+        this.loadFactor = loadFactor;
+        // threshold 初始为默认容量
+        threshold = (int)Math.min(capacity * loadFactor, MAXIMUM_CAPACITY + 1);
+        //初始化table数组
+        table = new Entry[capacity];
+        useAltHashing = sun.misc.VM.isBooted() &&
+                                           (capacity >= Holder.ALTERNATIVE_HASHING_THRESHOLD);
+        init();
+    }
+    * */
+    //public V put(K key, V value);
+    /*
+    public V put(K key, V value) {
+        //如果key是null，单独处理，存储到table[0]中，如果有另一个key为null，value覆盖
+        if (key == null)
+            return putForNullKey(value);
+        //对key的hashCode进行干扰，算出一个hash值
+
+//          hashCode值        xxxxxxxxxx
+//          table.length-1    000001111
+//
+//          hashCode值 xxxxxxxxxx  无符号右移几位和原来的hashCode值做^运算，使得hashCode高位二进制值参与计算，
+//                                也发挥作用，降低index冲突的概率。
+
+        int hash = hash(key);
+        //计算新的映射关系应该存到table[i]位置，
+        //i = hash & table.length-1，可以保证i在[0,table.length-1]范围内
+        int i = indexFor(hash, table.length);
+        //检查table[i]下面有没有key与我新的映射关系的key重复，如果重复替换value
+        for (Entry<K,V> e = table[i]; e != null; e = e.next) {
+            Object k;
+            if (e.hash == hash && ((k = e.key) == key || key.equals(k))) {
+                V oldValue = e.value;
+                e.value = value;
+                e.recordAccess(this);
+                return oldValue;
+            }
+        }
+
+        modCount++;
+        //添加新的映射关系
+        addEntry(hash, key, value, i);
+        return null;
+    }
+    * */
+    /*
+    * //如果key是null，直接存入[0]的位置
+    private V putForNullKey(V value) {
+        //判断是否有重复的key，如果有重复的，就替换value
+        for (Entry<K,V> e = table[0]; e != null; e = e.next) {
+            if (e.key == null) {
+                V oldValue = e.value;
+                e.value = value;
+                e.recordAccess(this);
+                return oldValue;
+            }
+        }
+        modCount++;
+        //把新的映射关系存入[0]的位置，而且key的hash值用0表示
+        addEntry(0, null, value, 0);
+        return null;
+    }
+    * */
+    /*
+    * final int hash(Object k) {
+        int h = 0;
+        if (useAltHashing) {
+            if (k instanceof String) {
+                return sun.misc.Hashing.stringHash32((String) k);
+            }
+            h = hashSeed;
+        }
+
+        h ^= k.hashCode();
+
+        // This function ensures that hashCodes that differ only by
+        // constant multiples at each bit position have a bounded
+        // number of collisions (approximately 8 at default load factor).
+        h ^= (h >>> 20) ^ (h >>> 12);
+        return h ^ (h >>> 7) ^ (h >>> 4);
+    }
+    * */
+    /*
+    * static int indexFor(int h, int length) {
+        return h & (length-1);
+    }
+    * */
+    /*
+    * void addEntry(int hash, K key, V value, int bucketIndex) {
+    //判断是否需要库容
+    //扩容：（1）size达到阈值（2）table[i]正好非空
+    if ((size >= threshold) && (null != table[bucketIndex])) {
+        //table扩容为原来的2倍，并且扩容后，会重新调整所有key-value的存储位置
+        resize(2 * table.length);
+        //新的key-value的hash和index也会重新计算
+        hash = (null != key) ? hash(key) : 0;
+        bucketIndex = indexFor(hash, table.length);
+    }
+	//存入table中
+    createEntry(hash, key, value, bucketIndex);
+}
+    * */
+    /*
+    * void createEntry(int hash, K key, V value, int bucketIndex) {
+        Entry<K,V> e = table[bucketIndex];
+        //原来table[i]下面的映射关系作为新的映射关系next
+        table[bucketIndex] = new Entry<>(hash, key, value, e);
+        //个数增加
+        size++;
+    }
+    * */
     //map.get(key1);
     /*
     * map.get(key1);
@@ -75,12 +254,11 @@ static class Entry<K,V> implements Map.Entry<K,V>
 
     ③ 如果table[index]不为空，那么就挨个比较哪个Entry的key与它相同，就删除它，把它前面的Entry的next的值修改为被删除Entry的next
     */
-
     @Test
-    public void test2(){
-        
+    public void test2() {
+
     }
-    
+
     /**
      * JDK8中
      */
@@ -104,7 +282,6 @@ jdk8时，底层的数据结构是：数组+单向链表+红黑树。
 复杂度为O(logn)比单向链表的时间复杂度O(n)的好
 
 */
-
 
 
 }
