dc65caf0ba06db7acca60c3a8a9cb925ed8ebe7d
359762406 <359762406@qq.com>
2023-08-21 10:57:20 +0800

    hashMap解析

--- chapter14_dataStructure/src/com/qihang03/map/HashMapTest.java
+++ chapter14_dataStructure/src/com/qihang03/map/HashMapTest.java
@@ -76,6 +76,34 @@ static class Entry<K,V> implements Map.Entry<K,V>
     ③ 如果table[index]不为空，那么就挨个比较哪个Entry的key与它相同，就删除它，把它前面的Entry的next的值修改为被删除Entry的next
     */
 
+    @Test
+    public void test2(){
+        
+    }
+    
+    /**
+     * JDK8中
+     */
+    /*
+①
+使用HashMap()的构造器创建对象时，并没有在底层初始化长度为16的table数组。
+
+②
+jdk8中添加的key,value封装到了HashMap.Node内部类 类的对象中,实现了Map.Entry的接口。而非jdk7中的HashMap.Entry。
+static class Node<K,V> implements Map.Entry<K,V>{};
+数组是Node[]
+③
+jdk8中新增的元素所在的索引位置如果有其他元素。在经过一系列判断后，如果能添加，
+    则是旧的元素指向新的元素（尾插法）。而非jdk7中的新的元素指向旧的元素（头插法）。“七上八下”
+    头插法可能在多线程模式，会导致死循环
+④
+jdk7时底层的数据结构是：数组+单向链表。
+jdk8时，底层的数据结构是：数组+单向链表+红黑树。
+红黑树出现的时机：当某个索引位置i上的链表的长度达到8，且数组的长度超过64时，此索引位置上的元素要从单向链表改为红黑树。
+如果索引i位置是红黑树的结构，当不断删除元素的情况下，当前索引i位置上的元素的个数低于6时，要从红黑树改为单向链表。
+复杂度为O(logn)比单向链表的时间复杂度O(n)的好
+
+*/
 
 
 
